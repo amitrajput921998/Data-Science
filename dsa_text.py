aec50e7919267ab10cbe8d41bda3503aaa526079
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-06-17 21:53:17 +0530

    Update dsa_text.py

--- dsa_text.py
+++ dsa_text.py
@@ -1,1 +1,24 @@
+def mySqrt(x):
+    if x == 0:
+        return 0
+
+    left = 1
+    right = x
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if mid * mid == x:
+            return mid
+        elif mid * mid > x:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return right
+
+
+x = int(input("Enter a non-negative integer: "))
+result = mySqrt(x)
+print("The square root of", x, "rounded down to the nearest integer is:", result)
 
